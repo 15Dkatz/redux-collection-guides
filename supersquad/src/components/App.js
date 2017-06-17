59b0081414b96ae46f0d2cf1a586a8658cec0f21
davidkatz <15Dkatz@shcp.edu>
2017-06-16 20:48:12 -0700

    add supersquad

---
+++ supersquad/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>SuperSquad</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
